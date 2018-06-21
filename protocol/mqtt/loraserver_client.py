3b4ccc2143d71d52e74cda24c0263ea909e7ffba
David Gomez Fernandez <david.gomezfernandez.external@atos.net>
2018-06-21 10:47:25 +0200

    LoRaServer real fix

--- protocol/mqtt/loraserver_client.py
+++ protocol/mqtt/loraserver_client.py
@@ -29,8 +29,8 @@ class LoRaServerClient (threading.Thread):
       self._logger = logging.getLogger(globals.BUS_NAME)       
       self._cayenne =   cayenne_parser.CayenneParser()
       self._active_timer = {}      
-      # self._thread = threading.Thread(target=self.Start, name="LoRaServer_thread")
-      self._thread = threading.Thread(target=self.TestParser, name="LoRaServer_thread")
+      self._thread = threading.Thread(target=self.Start, name="LoRaServer_thread")
+      # self._thread = threading.Thread(target=self.TestParser, name="LoRaServer_thread")
       self._thread.daemon = True
       self._thread.start()    
 
@@ -197,7 +197,7 @@ class LoRaServerClient (threading.Thread):
          "format": component.dictionary["RSSI"]["format"],
          "subscribed":False,
          "value": raw["rxInfo"][0]["rssi"],
-         "id": "SNR",
+         "id": "RSSI",
          "unit": component.dictionary["RSSI"]["unit"],
          "lastUpdate": str(raw["rxInfo"][0]["time"])}
       )
@@ -213,7 +213,7 @@ class LoRaServerClient (threading.Thread):
          "format": component.dictionary["Latitude"]["format"],
          "subscribed":False,
          "value": raw["rxInfo"][0]["latitude"],
-         "id": "SNR",
+         "id": "Latitude",
          "unit": component.dictionary["Latitude"]["unit"],
          "lastUpdate": str(raw["rxInfo"][0]["time"])}
       )
@@ -221,7 +221,7 @@ class LoRaServerClient (threading.Thread):
          "format": component.dictionary["Longitude"]["format"],
          "subscribed":False,
          "value": raw["rxInfo"][0]["longitude"],
-         "id": "SNR",
+         "id": "Longitude",
          "unit": component.dictionary["Longitude"]["unit"],
          "lastUpdate": str(raw["rxInfo"][0]["time"])}
       )
@@ -229,7 +229,7 @@ class LoRaServerClient (threading.Thread):
          "format": component.dictionary["Altitude"]["format"],
          "subscribed":False,
          "value": raw["rxInfo"][0]["altitude"],
-         "id": "SNR",
+         "id": "Altitude",
          "unit": component.dictionary["Altitude"]["unit"],
          "lastUpdate": str(raw["rxInfo"][0]["time"])}
       )
